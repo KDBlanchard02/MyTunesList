e9572c09beb2184024880c0efb1d382ec6b351bf
Kevin Blanchard <kevindanielblanchard@gmail.com>
2021-08-19 19:18:48 -0400

    Created Artist_Band.cs

---
+++ MyTunesList.Data/Artist_Band.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyTunesList.Data
+{
+    public class Artist_Band
+    {
+        [Key]
+        public int Artist_BandId { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        public List<string> Discography { get; set; }
+        
+        [Required]
+        public DateTime FormationDate { get; set; }
+
+        public double AverageRating { get; set; }
+
+        [Required]
+        public string Location { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+
+       // [Required]
+       // public enum Genre { get; set; }
+
+    }
+}
