de596d7ea88a9667359e046c6be7a7cdda08f8e2
Kevin Blanchard <kevindanielblanchard@gmail.com>
2021-08-19 21:26:19 -0400

    Added SingleRatingController.cs

---
+++ MyTunesList.WebAPI/Controllers/SingleRatingController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNet.Identity;
+using MyTunesList.Models;
+using MyTunesList.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace MyTunesList.WebAPI.Controllers
+{
+    [Authorize]
+    public class SingleRatingController : ApiController
+    {
+        private SingleRatingService CreateSingleRatingService()
+        {
+            var userId = Guid.Parse(User.Identity.GetUserId());
+            var singleRatingService = new SingleRatingService(userId);
+            return singleRatingService;
+        }
+
+        public IHttpActionResult Get()
+        {
+            SingleRatingService singleRatingService = CreateSingleRatingService();
+            var singleRatings = singleRatingService.GetSingleRatings();
+            return Ok(singleRatings);
+        }
+
+        public IHttpActionResult Post(SingleRatingCreate singleRating)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var service = CreateSingleRatingService();
+
+            if (!service.CreateSingleRating(singleRating))
+                return InternalServerError();
+
+            return Ok();
+        }
+    }
+}
