f6ead7ec2833bea9a390e8c100d36848ffc7d97e
Kevin Blanchard <kevindanielblanchard@gmail.com>
2021-08-19 21:04:26 -0400

    Added SingleRatingListItem, SingleRatingCreate, SingleRatingService

--- MyTunesList.Models/SingleRatingListItem.cs
+++ MyTunesList.Models/SingleRatingListItem.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,16 @@ namespace MyTunesList.Models
 {
     public class SingleRatingListItem
     {
+        public int SingleRatingId { get; set; }
 
+        [ForeignKey(nameof(Single))]
+        public int SingleId { get; set; }
+        public virtual Single Single { get; set; }
+
+        public double Rating { get; set; }
+
+        public string ReviewComment { get; set; }
+
+        public DateTimeOffset DateCreated { get; set; }
     }
 }
