86907366cc168b331297450d36f09a3e92b7f486
Kevin Blanchard <kevindanielblanchard@gmail.com>
2021-08-19 21:16:29 -0400

    Made additions to SingleRatingService.cs

--- MyTunesList.Services/SingleRatingService.cs
+++ MyTunesList.Services/SingleRatingService.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using MyTunesList.Data;
+using MyTunesList.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +10,50 @@ namespace MyTunesList.Services
 {
     public class SingleRatingService
     {
+        private readonly Guid _userId;
+
+        public SingleRatingService(Guid userId)
+        {
+            _userId = userId;
+        }
+
+        public bool CreateSingleRating(SingleRatingCreate model)
+        {
+            var entity = new SingleRating()
+            {
+                AuthorId = _userId,
+                Rating = model.Rating,
+                ReviewComment = model.ReviewComment,
+                DateCreated = DateTimeOffset.Now
+            };
+
+            using (var ctx = new ApplicationDbContext())
+            {
+                ctx.SingleRatings.Add(entity);
+                return ctx.SaveChanges() == 1;
+            }
+        }
+
+        public IEnumerable<SingleRatingListItem> GetSingleRatings()
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var query = ctx
+                    .SingleRatings
+                    .Where(e => e.AuthorId == _userId)
+                    .Select(
+                    e =>
+                    new SingleRatingListItem
+                    {
+                        SingleRatingId = e.SingleRatingId,
+                        SingleId = e.SingleId,
+                        Rating = e.Rating,
+                        ReviewComment = e.ReviewComment,
+                        DateCreated = e.DateCreated
+                    }
+                    );
+                return query.ToArray();
+            }
+        }
     }
 }
