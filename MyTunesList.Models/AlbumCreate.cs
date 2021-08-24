cb1743417ada57a539e30b3ca8a115e8a44d19b2
Catie Thomas <catherine.thomas.indy@gmail.com>
2021-08-24 19:26:48 -0400

    filled out albumrating and album create

--- MyTunesList.Models/AlbumCreate.cs
+++ MyTunesList.Models/AlbumCreate.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,19 @@ namespace MyTunesList.Models
 {
     public class AlbumCreate
     {
+        /*[Required]
+        public Artist_Band Artist { get; set; }*/
+
+        [Required]
+        public string AlbumTitle { get; set; }
+
+        [Required]
+        public double Length { get; set; }
+
+        [Required]
+        public DateTime ReleaseDate { get; set; }
+
+        public List<string> SongList { get; set; }
+
     }
 }
--- MyTunesList.Models/AlbumRatingCreate.cs
+++ MyTunesList.Models/AlbumRatingCreate.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,8 @@ namespace MyTunesList.Models
 {
     public class AlbumRatingCreate
     {
+        [Required]
+        public double Rating { get; set; }
+        public string ReviewComment { get; set; }
     }
 }
