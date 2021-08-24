b78464a08ba7818537c4359cd83f5994934a7b32
Catie Thomas <catherine.thomas.indy@gmail.com>
2021-08-24 19:23:02 -0400

    filled out albumrating and album detail

--- MyTunesList.Models/AlbumDetail.cs
+++ MyTunesList.Models/AlbumDetail.cs
@@ -8,5 +8,12 @@ namespace MyTunesList.Models
 {
     public class AlbumDetail
     {
+        public int AlbumId { get; set; }
+        public string AlbumTitle { get; set; }
+        public double Length { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public List<string> SongList { get; set; }
+        public double AverageRating { get; set; }
+        //public Artist_Band Artist {get; set;}
     }
 }
--- MyTunesList.Models/AlbumRatingDetail.cs
+++ MyTunesList.Models/AlbumRatingDetail.cs
@@ -8,5 +8,12 @@ namespace MyTunesList.Models
 {
     public class AlbumRatingDetail
     {
+        public int AlbumRatingId { get; set; }
+        public Guid AuthorId { get; set; }
+        //public Artist_Band Artist {get; set;}
+        public double Rating { get; set; }
+        public DateTimeOffset DateCreated { get; set; }
+        public DateTimeOffset? DateModified { get; set; }
+        public string ReviewComment { get; set; }
     }
 }
