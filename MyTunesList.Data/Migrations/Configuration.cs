da58034115828f7f907ebb6ad9ee7badcb24718e
Kevin Blanchard <kevindanielblanchard@gmail.com>
2021-08-26 19:13:12 -0400

    enabled migrations

---
+++ MyTunesList.Data/Migrations/Configuration.cs
@@ -0,0 +1,24 @@
+﻿namespace MyTunesList.Data.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<MyTunesList.Data.ApplicationDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+            ContextKey = "MyTunesList.Data.ApplicationDbContext";
+        }
+
+        protected override void Seed(MyTunesList.Data.ApplicationDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
