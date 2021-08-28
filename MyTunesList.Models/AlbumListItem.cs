f3cd827004bbc187aa2ce8de1d718ceb9e8b6a43
Catie Thomas <catherine.thomas.indy@gmail.com>
2021-08-28 12:02:04 -0400

    added albumcontroller, albumrating controller, albumlist item, and scaffolded controllers

---
+++ MyTunesList.Models/AlbumListItem.cs
@@ -0,0 +1,17 @@
+﻿using MyTunesList.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyTunesList.Models
+{
+    public class AlbumListItem
+    {
+        public int AlbumId { get; set; }
+        public string Title { get; set; }
+        public Artist_Band Artist { get; set; }
+        public DateTime ReleaseDate { get; set; }
+    }
+}
---
+++ MyTunesList.WebAPI/Controllers/AlbumController.cs
@@ -0,0 +1,58 @@
+﻿using MyTunesList.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Microsoft.AspNet.Identity;
+using MyTunesList.Models;
+
+namespace MyTunesList.WebAPI.Controllers
+{
+
+    public class AlbumController : ApiController
+    {
+        private RegularUserAlbumService CreateRegularUserAlbumService()
+        {
+            var userId = Guid.Parse(User.Identity.GetUserId());
+            var regularUserAlbumService = new RegularUserAlbumService(userId);
+            return regularUserAlbumService;
+        }
+
+        private AuthorizedUserAlbumService CreateAuthorizedUserAlbumService()
+        {
+            var userId = Guid.Parse(User.Identity.GetUserId());
+            var authorizedUserAlbumService = new AuthorizedUserAlbumService(userId);
+            return authorizedUserAlbumService;
+        }
+
+        public IHttpActionResult Get()
+        {
+            RegularUserAlbumService regularUserAlbumService = CreateRegularUserAlbumService();
+            var albums = regularUserAlbumService.GetAlbums();
+            return Ok(albums);
+        }
+
+        public IHttpActionResult Get(int id)
+        {
+            RegularUserAlbumService regularUserAlbumService = 
+        }
+
+        public IHttpActionResult Post(AlbumCreate album)
+        {
+
+        }
+
+        public IHttpActionResult Put(AlbumEdit album)
+        {
+
+        }
+
+        public IHttpActionResult Delete(int id)
+        {
+
+        }
+    }
+
+}
