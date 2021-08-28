b52dbb976e4b20262600ccb2c786fbb4662a76fe
Kevin Blanchard <kevindanielblanchard@gmail.com>
2021-08-28 12:36:28 -0400

    Saved changes

---
+++ MyTunesList.Data/Migrations/202108281635355_AddedSqlReaderVOne.cs
@@ -0,0 +1,16 @@
+﻿namespace MyTunesList.Data.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddedSqlReaderVOne : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
