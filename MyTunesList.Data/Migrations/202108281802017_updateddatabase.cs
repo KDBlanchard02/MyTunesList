4540bc1d71e0e059840a702b2680cd3c490af8a9
Kevin Blanchard <kevindanielblanchard@gmail.com>
2021-08-28 14:20:33 -0400

    saved changes

---
+++ MyTunesList.Data/Migrations/202108281802017_updateddatabase.cs
@@ -0,0 +1,18 @@
+﻿namespace MyTunesList.Data.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class updateddatabase : DbMigration
+    {
+        public override void Up()
+        {
+            DropColumn("dbo.SingleTrack", "AverageRating");
+        }
+        
+        public override void Down()
+        {
+            AddColumn("dbo.SingleTrack", "AverageRating", c => c.Double(nullable: false));
+        }
+    }
+}
